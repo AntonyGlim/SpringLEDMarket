b1b5b63985aa2cf6cac29e28f68e279f47eef7d4
Antony.Glim <Antony.Glim@gmail.com>
2019-09-14 13:42:32 +0300

    ProductsController add

--- src/main/java/glim/antony/spring_led_market/controllers/ProductsController.java
+++ src/main/java/glim/antony/spring_led_market/controllers/ProductsController.java
@@ -1,4 +1,14 @@
 package glim.antony.spring_led_market.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/products")
 public class ProductsController {
+    public String showProducts(Model model){
+        return "products";
+    }
 }
+
