4c128f3154be2c7ca065d84ff56616634de8ff95
Antony.Glim <Antony.Glim@gmail.com>
2019-09-14 11:51:15 +0300

    market in working condition

--- src/main/java/glim/antony/spring_led_market/controllers/MainController.java
+++ src/main/java/glim/antony/spring_led_market/controllers/MainController.java
@@ -1,4 +1,12 @@
 package glim.antony.spring_led_market.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
