04b2c8bad331dc9ecc6a107e91c1ed077a456e2a
Antony.Glim <Antony.Glim@gmail.com>
2019-09-14 11:32:23 +0300

    Configuration class add

---
+++ src/main/java/glim/antony/spring_led_market/config/AppConfig.java
@@ -0,0 +1,9 @@
+package glim.antony.spring_led_market.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("glim.antony.spring_led_market")
+public class AppConfig {
+}
