9263004c727d6c88b14cd28bad1d88cc66870888
Antony.Glim <Antony.Glim@gmail.com>
2019-09-14 10:56:07 +0300

    files for initial commit add

---
+++ src/main/java/glim/antony/spring_led_market/SpringLedMarketApplication.java
@@ -0,0 +1,13 @@
+package glim.antony.spring_led_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLedMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLedMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/glim/antony/spring_led_market/SpringLedMarketApplicationTests.java
@@ -0,0 +1,16 @@
+package glim.antony.spring_led_market;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringLedMarketApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
