0aa2efdbb723d6543bfbc5dff31ef4a986765504
Antony.Glim <Antony.Glim@gmail.com>
2019-09-14 13:08:34 +0300

    ProductSpecifications create and add

---
+++ src/main/java/glim/antony/spring_led_market/controllers/ProductsController.java
@@ -0,0 +1,4 @@
+package glim.antony.spring_led_market.controllers;
+
+public class ProductsController {
+}
---
+++ src/main/java/glim/antony/spring_led_market/repositories/specifications/ProductSpecifications.java
@@ -0,0 +1,22 @@
+package glim.antony.spring_led_market.repositories.specifications;
+
+import glim.antony.spring_led_market.entities.Product;
+import org.springframework.data.jpa.domain.Specification;
+
+public class ProductSpecifications {
+    public static Specification<Product> titleContains(String word) {
+        return (Specification<Product>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(root.get("title"), "%" + word + "%");
+    }
+
+    public static Specification<Product> priceGreaterThanOrEq(double value) {
+        return (Specification<Product>) (root, criteriaQuery, criteriaBuilder) -> {
+            return criteriaBuilder.greaterThanOrEqualTo(root.get("price"), value);
+        };
+    }
+
+    public static Specification<Product> priceLesserThanOrEq(double value) {
+        return (Specification<Product>) (root, criteriaQuery, criteriaBuilder) -> {
+            return criteriaBuilder.lessThanOrEqualTo(root.get("price"), value);
+        };
+    }
+}
