f08511287b527d5ab49d76bc8c796aa3caa19b0d
Antony.Glim <Antony.Glim@gmail.com>
2019-09-15 14:09:06 +0300

    products list without filters shown on page.

--- src/main/java/glim/antony/spring_led_market/controllers/ProductsController.java
+++ src/main/java/glim/antony/spring_led_market/controllers/ProductsController.java
@@ -1,13 +1,31 @@
 package glim.antony.spring_led_market.controllers;
 
+import glim.antony.spring_led_market.entities.Product;
+import glim.antony.spring_led_market.services.ProductsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/products")
 public class ProductsController {
+
+    ProductsService productsService;
+
+    @Autowired
+    public void setProductsService(ProductsService productsService) {
+        this.productsService = productsService;
+    }
+
+    @GetMapping("/")
     public String showProducts(Model model){
+        Specification<Product> spec = Specification.where(null);
+
+        model.addAttribute("page", productsService.findAllByPaginAndFiltering(spec, PageRequest.of(0, 5)));
         return "products";
     }
 }
