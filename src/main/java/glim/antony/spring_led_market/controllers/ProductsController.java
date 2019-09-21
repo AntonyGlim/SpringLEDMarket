8df555ef0f9836dca35fc1042f4d83c8be15d92a
Antony.Glim <Antony.Glim@gmail.com>
2019-09-21 11:14:34 +0300

    cosmetic changes

--- src/main/java/glim/antony/spring_led_market/controllers/ProductsController.java
+++ src/main/java/glim/antony/spring_led_market/controllers/ProductsController.java
@@ -1,25 +1,20 @@
 package glim.antony.spring_led_market.controllers;
 
 import glim.antony.spring_led_market.entities.Product;
-import glim.antony.spring_led_market.repositories.specifications.ProductSpecifications;
 import glim.antony.spring_led_market.services.ProductsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import javax.swing.text.html.Option;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/products")
 public class ProductsController {
 
-    ProductsService productsService;
+    private ProductsService productsService;
 
     @Autowired
     public void setProductsService(ProductsService productsService) {
@@ -27,21 +22,18 @@ public class ProductsController {
     }
 
     @GetMapping()
-    public String showProducts(Model model,
-                               @RequestParam(name = "word", required = false) String word,
-                               @RequestParam(name = "min", required = false) Integer min,
-                               @RequestParam(name = "max", required = false) Integer max,
-                               @RequestParam(name = "pageNumber", required = false) Integer pageNumber)
+    public String showProducts(
+            Model model,
+            @RequestParam(name = "word", required = false) String word,
+            @RequestParam(name = "min", required = false) Integer min,
+            @RequestParam(name = "max", required = false) Integer max,
+            @RequestParam(name = "pageNumber", required = false) Integer pageNumber)
     {
-        productsService.setParams(word, min, max, pageNumber);
-
-        //TODO transfer Specification to service
-
-        if (pageNumber == null){
-            pageNumber = 1;
-        }
-
-        Page<Product> page = productsService.findAllByPaginAndFiltering(PageRequest.of(pageNumber - 1, 5, Sort.Direction.ASC, "id"));
+        productsService.setParams(word, min, max);
+        if (pageNumber == null) pageNumber = 1;
+        Page<Product> page =
+                productsService.findAllByPaginAndFiltering(
+                        PageRequest.of(pageNumber - 1, 5, Sort.Direction.ASC, "id"));
         model.addAttribute("page", page);
         model.addAttribute("word", word);
         model.addAttribute("min", min);
--- src/main/java/glim/antony/spring_led_market/services/ProductsService.java
+++ src/main/java/glim/antony/spring_led_market/services/ProductsService.java
@@ -15,29 +15,35 @@ import java.util.List;
 @Service
 public class ProductsService {
 
-    String word;
-    Integer min;
-    Integer max;
-    Integer pageNumber;
-
-    ProductsRepository productsRepository;
+    private String word;
+    private Integer min;
+    private Integer max;
+    private ProductsRepository productsRepository;
 
     @Autowired
     public void setProductsRepository(ProductsRepository productsRepository) {
         this.productsRepository = productsRepository;
     }
 
+    public void setParams(String word, Integer min, Integer max) {
+        this.word = word;
+        this.min = min;
+        this.max = max;
+    }
+
+    public String getFilterStringForURL(){
+        StringBuilder stringBuilder = new StringBuilder();
+        if (word != null) stringBuilder.append("&word=").append(word);
+        if (word != null) stringBuilder.append("&min=").append(min);
+        if (word != null) stringBuilder.append("&max=").append(max);
+        return stringBuilder.toString();
+    }
+
     public Page<Product> findAllByPaginAndFiltering(Pageable pageable){
         Specification<Product> spec = Specification.where(null);
-        if (word != null){
-            spec = spec.and(ProductSpecifications.titleContains(word));
-        }
-        if (min != null) {
-            spec = spec.and(ProductSpecifications.priceGreaterThanOrEq(min));
-        }
-        if (max != null){
-            spec = spec.and(ProductSpecifications.priceLesserThanOrEq(max));
-        }
+        if (word != null) spec = spec.and(ProductSpecifications.titleContains(word));
+        if (min != null) spec = spec.and(ProductSpecifications.priceGreaterThanOrEq(min));
+        if (max != null) spec = spec.and(ProductSpecifications.priceLesserThanOrEq(max));
         return productsRepository.findAll(spec, pageable);
     }
 
@@ -59,24 +65,4 @@ public class ProductsService {
         return list;
     }
 
-    public void setParams(String word, Integer min, Integer max, Integer pageNumber) {
-        this.word = word;
-        this.min = min;
-        this.max = max;
-        this.pageNumber = pageNumber;
-    }
-
-    public String getFilterStringForURL(){
-        StringBuilder stringBuilder = new StringBuilder();
-        if (word != null){
-            stringBuilder.append("&word=" + word);
-        }
-        if (min != null) {
-            stringBuilder.append("&min=" + min);
-        }
-        if (max != null){
-            stringBuilder.append("&max=" + max);
-        }
-        return stringBuilder.toString();
-    }
 }
