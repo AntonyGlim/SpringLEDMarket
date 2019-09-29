294bc0e2b00caa7d15b8d586843ba11536baf46b
Antony.Glim <Antony.Glim@gmail.com>
2019-09-29 17:29:14 +0300

    RoleRepository and UserRepository add

---
+++ src/main/java/glim/antony/spring_led_market/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package glim.antony.spring_led_market.repositories;
+
+import glim.antony.spring_led_market.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findOneByUsername(String username);
+}
