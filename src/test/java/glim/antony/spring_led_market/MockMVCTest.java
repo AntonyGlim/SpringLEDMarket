a0f42222e116e40a5695265e150476219d539bf3
Antony.Glim@gmail.com <Antony.Glim@gmail.com>
2019-11-06 19:45:28 +0300

    Add several tests

--- src/test/java/glim/antony/spring_led_market/MockMVCTest.java
+++ src/test/java/glim/antony/spring_led_market/MockMVCTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -34,6 +35,14 @@ public class MockMVCTest {
                 .andExpect(content().string(containsString("интернет-магазин")));
     }
 
+    @Test //есть-ли на том адресе на который обращаемся на странице такие слова
+    public void tryToLoadCart() throws Exception{
+        mockMvc.perform(get("/cart"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Корзина")));
+    }
+
     @Test //стучим на админ, знаем что не должно нас пустить но должно перекинуть на определенный адрес
     public void securityAccessDeniedTest() throws Exception {
         mockMvc.perform(get("/admin"))
@@ -47,14 +56,30 @@ public class MockMVCTest {
         mockMvc.perform(formLogin("/authenticateTheUser").user("admin").password("100"))
                 .andDo(print())
                 .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/"));
+                .andExpect(redirectedUrl("/login?error"));
     }
 
     @Test
     public void badCredentials() throws Exception {
-        // todo скорректировать
         mockMvc.perform(formLogin("/authenticateTheUser").user("moo").password("moo"))
                 .andDo(print())
-                .andExpect(status().isForbidden());
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    public void authTest() throws Exception {
+        mockMvc.perform(formLogin("/authenticateTheUser").user("88888888").password("100"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
+
+    @Test
+    @WithMockUser(username = "+79380000000", roles = "ADMIN")
+    // @WithAnonymousUser
+    public void securityAccessAllowedTest() throws Exception {
+        mockMvc.perform(get("/admin"))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
     }
 }
